f607b168f1ca1aaf4571bc9450c043155082dba2
rawi-FHGR <ralph.wildhaber@stud.fhgr.ch>
2025-05-15 00:39:19 +0200

    logger and callback suppression added.

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import dash
 import dash_bootstrap_components as dbc
+import logs.logging_config
 
 # project specific imports
 import layout as layout
 from callbacks import register_callbacks
 
 # setup dash application
-app = dash.Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__,
+                external_stylesheets = [dbc.themes.BOOTSTRAP],
+                suppress_callback_exceptions=True)      # due to dynamic component generation
 
 # apply the layout to the app
 app.layout = layout.create_layout()
