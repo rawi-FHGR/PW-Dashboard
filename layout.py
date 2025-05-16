c3093f91e1736b570fd971d91a8dfaed1534e19b
Lukas <lukas.temperli@bluewin.ch>
2025-05-16 14:06:18 +0200

    created layout to match figma template

--- layout.py
+++ layout.py
@@ -12,13 +12,15 @@ texts = {'page.title':'Personenwagen-Dashboard',
 # functions
 def create_layout():
         return html.Div([
+            dcc.Tabs([
+            dcc.Tab(label='Bestand', children=[
             html.H1(texts['page.title'], style={'textAlign': 'center'}),
 
             # 1st line: CH map and canton / municipality map
             html.Div([
-                dcc.Graph(id='choropleth-map', style={'width': '50%'}),
+                dcc.Graph(id='choropleth-map', style={'width': '50%', 'border': '1px solid black','boxSizing': 'border-box','border-radius': '25px','overflow': 'hidden'}),
                 dcc.Graph(id='choropleth-map-municipality',
-                          figure=generate_map_municipality(2023, 'ZH'), style={'width': '50%'})
+                          figure=generate_map_municipality(2023, 'ZH'), style={'width': '50%', 'border': '1px solid black','boxSizing': 'border-box','border-radius': '25px','overflow': 'hidden'})
             ], style={
                 'display': 'flex',
                 'flexDirection': 'row',  # nebeneinander
@@ -30,6 +32,7 @@ def create_layout():
             }),
 
             # 2nd line: year slider
+            html.Div(
             html.Div(
                 dcc.Slider(
                     id='year-slider',
@@ -44,14 +47,15 @@ def create_layout():
                     tooltip={"placement": "bottom", "always_visible": True},
                     className='custom-slider'
                 ),
-                style={'width': '25%', 'margin': '20px auto'}
+                style={'width': '50%', 'margin': '20px auto'}
             ),
+        style={'background-color': 'white','border': '1px solid black','boxSizing': 'border-box','border-radius': '25px','overflow': 'hidden'}),
 
             # additional graphs (3 side-by-side)
             html.Div([
-                html.Div(dcc.Graph(id='stackedbar-fuel-ivs', style={'height': '100%'}), style={'width': '33%'}),
-                html.Div(dcc.Graph(id='stackedbar-fuel-stock', style={'height': '100%'}), style={'width': '33%'}),
-                html.Div(dcc.Graph(id='multiline-total', style={'height': '100%'}), style={'width': '33%'})
+                html.Div(dcc.Graph(id='stackedbar-fuel-ivs', style={'height': '100%'}), style={'width': '33.333%','border': '1px solid black','boxSizing': 'border-box','border-radius': '25px','overflow': 'hidden'}),
+                html.Div(dcc.Graph(id='stackedbar-fuel-stock', style={'height': '100%'}), style={'width': '33.333%','border': '1px solid black','boxSizing': 'border-box','border-radius': '25px','overflow': 'hidden'}),
+                html.Div(dcc.Graph(id='multiline-total', style={'height': '100%'}), style={'width': '33.333%','border': '1px solid black','boxSizing': 'border-box','border-radius': '25px','overflow': 'hidden'})
             ], style={
                 'display': 'flex',
                 'flexDirection': 'row',
@@ -59,6 +63,57 @@ def create_layout():
                 'alignItems': 'stretch',
                 'width': '100%',
                 'gap': '10px',
-                'padding': '10px'
+                'padding-top': '10px'
+
             })
-        ])
+        ],
+                    style={
+                        'padding': '12px',
+                        'fontSize': '18px',
+                        'fontWeight': 'bold',
+                        'backgroundColor': 'white',
+                        'color': 'black',
+                        'border': '2px solid black',
+                        'border-radius-top-right':'50px',
+                        'border-top-right-radius': '50px',
+                    },
+                    selected_style={
+                        'padding': '12px',
+                        'fontSize': '18px',
+                        'fontWeight': 'bold',
+                        'backgroundColor': 'red',
+                        'color': 'black',
+                        'border': '2px solid black',
+                        'borderBottom': 'none',
+                        'border-top-right-radius':'50px',
+                    }
+                    ),
+        dcc.Tab(label='Inverkehrssetzungen', children=[
+            html.Div("andere Inhalte", style={'padding':'20px'})
+            ]
+                ,
+                style={
+                    'padding': '12px',
+                        'fontSize': '18px',
+                        'fontWeight': 'bold',
+                        'backgroundColor': 'white',
+                        'color': 'black',
+                        'border': '2px solid black',
+                        'border-radius-top-right':'50px',
+                        'border-top-left-radius': '50px',
+                },
+                selected_style={
+                    'padding': '12px',
+                    'fontSize': '18px',
+                    'fontWeight': 'bold',
+                    'backgroundColor': 'red',
+                    'color': 'black',
+                    'border': '2px solid black',
+                    'borderBottom': 'none',
+                    'border-top-right-radius':'50px',
+                }
+                ,
+                )
+            ])
+            ], style={'backgroundColor': 'red'}
+        )
